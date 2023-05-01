ab9c96d9941134eb86fb01f44ed41ce1e50dacee
Peter Yockey <peterjyockey@gmail.com>
2023-05-01 16:31:46 -0500

    added /api/notes get, shows db values in left of webpage

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const path = require('path');
-// const api = require('./Develop/public/assets/js/index.js');
+const fs = require('fs');
+const db = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use('/api', api);
 
 
 app.get('/', (req, res) => 
-    res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-
-
+app.get('/api/notes', (req, res) => 
+    res.json(db)
+);
 
 
 
